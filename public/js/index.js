fcb3d405ea88fde2c15c14b46ffa878d61644862
Carl <stringcarlroxas@gmail.com>
2022-08-23 15:00:14 +0800

    Heroku Integration

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+let socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to the server.');
+    
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from the server.');
+})
+
+socket.on('newMessage', function (message) {
+    console.log("newMessage", message);
+});
